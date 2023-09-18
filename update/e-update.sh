e1366f66fd6a5231d7cf0d67d802d26d2c1f8f3e
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-18 13:51:58 +0700

    Update and rename update.sh to e-update.sh

--- update/e-update.sh
+++ update/e-update.sh
@@ -6,6 +6,7 @@ echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/hokagelegend2023/echo/main/menu/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /root/log-install.txt https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/log-install.txt
+wget -q -O /usr/bin/e-update "https://raw.githubusercontent.com/hokagelegend2023/echo/main/update/e-update.sh" && chmod +x /usr/bin/e-update
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
