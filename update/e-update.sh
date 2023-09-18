e0f8af3e9c89d7dcdfa78e9284ba2bc69f03fc0e
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-18 13:57:07 +0700

    Update e-update.sh

--- update/e-update.sh
+++ update/e-update.sh
@@ -2,6 +2,11 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
+echo -e " [INFO] Hapus File Sebelum Nya..."
+cd /usr/bin/
+rm menu
+rm e-update
+
 echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/hokagelegend2023/echo/main/menu/menu.sh" && chmod +x /usr/bin/menu
