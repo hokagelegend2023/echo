3772cd44281205f93a836952c8e7b83283a1fd97
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-18 14:02:47 +0700

    Update e-update.sh

--- update/e-update.sh
+++ update/e-update.sh
@@ -11,7 +11,7 @@ rm info
 echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/hokagelegend2023/echo/main/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /root/log-install.txt https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/log-install.txt
+wget -q -O /root/log-install.txt "https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/log-install.txt"
 wget -q -O /usr/bin/e-update "https://raw.githubusercontent.com/hokagelegend2023/echo/main/update/e-update.sh" && chmod +x /usr/bin/e-update
 wget -q -O /usr/bin/info "https://raw.githubusercontent.com/hokagelegend2023/echo/main/update/info.sh" && chmod +x /usr/bin/info
 echo -e " [INFO] Update Successfully"
