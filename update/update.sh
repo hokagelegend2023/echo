3995e300fd0a40ceddabc78418c37af4c80df95a
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-18 13:42:34 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -5,6 +5,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/hokagelegend2023/echo/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /root/log-install.txt https://raw.githubusercontent.com/hokagelegend2023/vpnpremium/main/update/log-install.txt
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
